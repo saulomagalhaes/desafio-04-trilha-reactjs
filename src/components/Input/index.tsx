ab9ac0d6ca1867a99ef6e6bd1adccee99fd33559
Saulo Magalhães <saulo.magalhaes@outlook.com.br>
2022-04-22 19:08:24 -0300

    feat: falta dashboard e modaleditfood

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,20 +1,15 @@
-import {
-  useEffect,
-  useRef,
-  useState,
-  useCallback,
-} from 'react';
-
-type InputProps = {
-  name: string;
-  icon?: React.ComponentType<{ size: number }>;
-}
-
+import { useEffect, useRef, useState, useCallback } from 'react';
 import { useField } from '@unform/core';
 
 import { Container } from './styles';
 
-const Input = ({ name, icon: Icon, ...rest }:InputProps) => {
+type InputProps = {
+  name: string;
+  placeholder: string;
+  icon?: React.ComponentType<{ size: number }>;
+};
+
+const Input = ({ name, icon: Icon, ...rest }: InputProps) => {
   const inputRef = useRef<any>(null);
 
   const [isFocused, setIsFocused] = useState(false);
