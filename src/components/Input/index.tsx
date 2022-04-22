98be0bece850a072fa5417330efb979d136434e3
Saulo Magalhães <saulo.magalhaes@outlook.com.br>
2022-04-22 16:38:45 -0300

    feat: fiz ate o Modal

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -5,12 +5,17 @@ import {
   useCallback,
 } from 'react';
 
+type InputProps = {
+  name: string;
+  icon?: React.ComponentType<{ size: number }>;
+}
+
 import { useField } from '@unform/core';
 
 import { Container } from './styles';
 
-const Input = ({ name, icon: Icon, ...rest }) => {
-  const inputRef = useRef(null);
+const Input = ({ name, icon: Icon, ...rest }:InputProps) => {
+  const inputRef = useRef<any>(null);
 
   const [isFocused, setIsFocused] = useState(false);
   const [isFilled, setIsFilled] = useState(false);
---
+++ src/components/Modal/index.tsx
@@ -0,0 +1,48 @@
+import { useEffect, useState } from 'react';
+import ReactModal from 'react-modal';
+
+type ModalProps = {
+  isOpen: boolean;
+  children: React.ReactNode;
+  setIsOpen: () => void;
+};
+
+function Modal(props: ModalProps) {
+  const { isOpen, children, setIsOpen } = props;
+  const [modalStatus, setModalStatus] = useState(false);
+
+  useEffect(() => {
+    return setModalStatus(isOpen);
+  }, [isOpen]);
+
+  return (
+    <ReactModal
+      shouldCloseOnOverlayClick={!false}
+      onRequestClose={setIsOpen}
+      isOpen={modalStatus}
+      ariaHideApp={false}
+      style={{
+        content: {
+          top: '50%',
+          left: '50%',
+          right: 'auto',
+          bottom: 'auto',
+          marginRight: '-50%',
+          transform: 'translate(-50%, -50%)',
+          background: '#F0F0F5',
+          color: '#000000',
+          borderRadius: '8px',
+          width: '736px',
+          border: 'none',
+        },
+        overlay: {
+          backgroundColor: '#121214e6',
+        },
+      }}
+    >
+      {children}
+    </ReactModal>
+  );
+}
+
+export default Modal;
